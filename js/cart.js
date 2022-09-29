16fdd59415ac2b11b86ba7800a22e7b06a4ea005
oassem <noneed762@gmail.com>
2022-09-30 00:31:18 +0200

    Added cart.html, cart.js, and cart.css to project files

--- js/cart.js
+++ js/cart.js
@@ -1,251 +1,126 @@
-var ID=JSON.parse(localStorage.getItem("selectedID"));
-var totalPriceOfAllObjectsInCart =0 ;
-console.log(`Selected id is ${ID}`);
-if (localStorage.getItem("products")) {
-    objectsArr = JSON.parse(localStorage.getItem("products"));
+// check if cart is already created
+if (localStorage.getItem("cart") === null) {
+    let cartArr = [];
+    localStorage.setItem("cart", JSON.stringify(cartArr));
 }
 
-objectsArr.forEach(product=>{
-  
-    for(var key in product){
-       
-        if (key=='id'){
-          
-            if(product.id==ID){
-                console.log(product);
-                cartItem={
-                    remove: 0,
-                    Image:product.image,
-                    Product:product.title,
-                    Price:product.price,
-                    Quantity:1,
-                    Total:product.price
-                };
-                var cart=JSON.parse(localStorage.getItem('cart_items'));
-                
-                if(cart==null) cart=[];
-                cart.push(cartItem);
-                cart=localStorage.setItem("cart_items",JSON.stringify(cart));
-                // console.log(product);
-              
-            }
-        }
-    }});
+// initalize some variables
+let totalPriceOfAllObjectsInCart = 0;
+let objectsArr = JSON.parse(localStorage.getItem("products"));
+let tempArr = JSON.parse(localStorage.getItem("cart"));
+let flag = false;
 
-let cartArr = [];
-if (JSON.parse(localStorage.getItem("cart_items"))) {
-    cartArr = JSON.parse(localStorage.getItem("cart_items"));
+// check if user has selected new item
+if (JSON.parse(localStorage.getItem("selectedID")) != null) {
+    let ID = JSON.parse(localStorage.getItem("selectedID"));
+    tempArr.forEach(function (arrayItem) {
+        if (arrayItem.id == ID) { flag = true; }
+    });
+    if (!flag) {
+        let selectedItem = objectsArr.find((item) => item.id == ID);
+        selectedItem.quantity = 1;
+        tempArr.push(selectedItem);
+        localStorage.setItem("cart", JSON.stringify(tempArr));
+    }
+    localStorage.setItem("selectedID", null);
 }
-// cartArr.forEach(element => {
-//     console.log(element);
-// });
 
-let tbody = document.getElementById("tbody");
-let bsection= document.getElementById("cart-bottom");
+function setPageContent() {
+    let tbody = document.getElementById("tbody");
+    let bsection = document.getElementById("cart-bottom");
+    tbody.innerHTML = null;
+    bsection.innerHTML = null;
 
-//loop on array of objects in cart :) 
-cartArr.forEach((element,index)=>{
-    
-       let tr = document.createElement('tr');
-       // add remove button 
-      
-     console.log(index);
-       let remove_td = document.createElement('td');
-       remove_td.innerHTML =`<i data-id="${ID}" onclick="removeElement('${index}')" class="remove_btn  fas fa-times-circle" aria-hidden="true"></i>`;
+    tempArr.forEach((e, index) => {
+        let tr = document.createElement('tr');
+        let remove_td = document.createElement('td');
+        remove_td.innerHTML = `<i data-id="${e.id}" onclick="removeElement('${index}')" class="remove_btn  fas fa-times-circle" aria-hidden="true"></i>`;
         tr.appendChild(remove_td);
 
-       // add image
+        //add image
         let img_td = document.createElement('td');
-        img_td.innerHTML =`<img class="card-img-top" src="${element.Image}">`;
-        tr.appendChild(img_td) ;  
+        img_td.innerHTML = `<img class="card-img-top" src="${e.image}">`;
+        tr.appendChild(img_td);
 
-
-        //  add title 
+        //add title 
         let title_td = document.createElement('td');
-        title_td.textContent = element.Product;
+        title_td.textContent = e.title;
         tr.appendChild(title_td);
 
-         //item price
+        //item price
         let price_td = document.createElement("td");
-        price_td.textContent = element.Price+" L.E";
+        price_td.textContent = e.price + " EGP";
         tr.appendChild(price_td);
 
         //items quantity
         let qty_td = document.createElement('td');
-        qty_td.innerHTML =`<i data-id="${ID}" class="decrease fas fa-minus-circle" aria-hidden="true" ></i>
-                           <span >${element.Quantity}</span>
-                           <i data-id="${ID}" class="increase fas fa-plus-circle" aria-hidden="true" ></i>`;
+        qty_td.innerHTML = `<i data-id="${e.id}" class="decrease fas fa-minus-circle" aria-hidden="true" onclick='dec(${index})'></i>
+                               <span>${e.quantity}</span>
+                               <i data-id="${e.id}" class="increase fas fa-plus-circle" aria-hidden="true" onclick='inc(${index})'></i>`;
         tr.appendChild(qty_td);
 
-
-          //total per item  
+        //total per item  
         let total_td = document.createElement("td");
-        total_td.textContent = element.Price * element.Quantity +" L.E";
+        total_td.textContent = e.price * e.quantity + " EGP";
         tr.appendChild(total_td);
-        totalPriceOfAllObjectsInCart+= parseInt(element.Price * element.Quantity);
         tbody.appendChild(tr);
-        console.log(totalPriceOfAllObjectsInCart);
-       
-        
-});
-console.log(totalPriceOfAllObjectsInCart);
+    });
 
-var cartPrice=localStorage.setItem('totalPriceofAllObjectsInCart',totalPriceOfAllObjectsInCart);
-
-var cartPrice=localStorage.getItem('totalPriceofAllObjectsInCart');
-console.log(cartPrice);
-
-let b_title = document.createElement('div');
-b_title.innerHTML = `<div class="total col-lg-6 col-md-6 col-12">
+    let b_title = document.createElement('div');
+    b_title.innerHTML = `<div class="total col-lg-6 col-md-6 col-12">
     <h5>Total</h5>
     <div class="title d-flex justify-content-between">
         <h6>Subtotal</h6>
-        <p>${parseInt(cartPrice)} L.E</p>
+        <p>${getTotalPrice()} EGP</p>
     </div>
 
     <div class="title d-flex justify-content-between">
         <h6>Shipping</h6>
-        <p>50 L.E</p>
+        <p>50 EGP</p>
     </div>
     <hr class="second-hr">
-    <div class="title d-flex justify-content-between">
+    <div class="title d-flex justify-content-between mb-2">
         <h6>Total</h6>
-        <p>${parseInt(cartPrice)+50} L.E</p>
+        <p>${totalPriceOfAllObjectsInCart + 50} EGP</p>
     </div> 
-    <a href="index.html"><button  class="home_btn btn btn-primary mt-1 ml-auto" > <i class="fa fa-arrow-left" aria-hidden="true"></i> Back To Home</button></a> 
-    <a href="checkout.html"><button class="proceed_btn btn btn-primary mt-1 ">Proceed To checkout</button></a> 
-</div>`;
-bsection.appendChild(b_title);
-// **********************************
-//remove element in cart 
-function removeElement(index){
-   // console.log(index);// index of object
-cartArr=JSON.parse(localStorage.getItem("cart_items"));
-removedItemPrice=cartArr[index];
-for(var key in removedItemPrice){
-    if(key=='Price'){
-        removedItemPrice=removedItemPrice[key];
-        console.log(removedItemPrice);
+    <a href="index.html"><button class="home_btn btn btn-primary mt-1 ml-auto"></i> Back To Home</button></a> 
+    <button class="proceed_btn btn btn-primary mt-1" onclick='cartRedirect()'>Proceed To checkout</button>
+    </div>`;
+    bsection.appendChild(b_title);
+}
+
+
+function getTotalPrice() {
+    totalPriceOfAllObjectsInCart = 0;
+    tempArr.forEach(function (arrayItem) {
+        totalPriceOfAllObjectsInCart += arrayItem.price * arrayItem.quantity;
+    });
+    return totalPriceOfAllObjectsInCart;
+}
+
+function removeElement(index) {
+    tempArr.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(tempArr));
+    setPageContent();
+}
+
+function dec(index) {
+    if (tempArr[index].quantity > 1) {
+        tempArr[index].quantity -= 1;
+        localStorage.setItem("cart", JSON.stringify(tempArr));
+        setPageContent();
     }
 }
-// console.log(removedItemPrice);
-// console.log(totalPriceOfAllObjectsInCart);
 
-cartArr.splice(index,1);
-
-localStorage.setItem("cart_items",JSON.stringify(cartArr));
-totalPriceOfAllObjectsInCart= totalPriceOfAllObjectsInCart - removedItemPrice;
-console.log(totalPriceOfAllObjectsInCart);
-
-   cartPrice=localStorage.setItem('totalPriceofAllObjectsInCart',JSON.stringify(totalPriceOfAllObjectsInCart));
- 
-cartPrice=JSON.parse(localStorage.getItem('totalPriceofAllObjectsInCart'));
-cartPrice=parseInt(cartPrice);
-console.log(cartPrice);
-
-// show items in the screen after delete :)
-
-let tbody = document.getElementById("tbody");
-let bsection= document.getElementById("cart-bottom");
-tbody.innerHTML=null;
-bsection.innerHTML=null;
-  cartArr.forEach(element=>{
-    console.log(element);
-    let tr = document.createElement('tr');
-    // add remove button 
-    let remove_td = document.createElement('td');
-    remove_td.innerHTML =`<i data-id="${ID}" onclick="removeElement('${element.Product}')" class="remove_btn  fas fa-times-circle" aria-hidden="true"></i>`;
-     tr.appendChild(remove_td);
-
-    // add image
-     let img_td = document.createElement('td');
-     img_td.innerHTML =`<img class="card-img-top" src="${element.Image}">`;
-     tr.appendChild(img_td) ;  
-
-
-     //  add title 
-     let title_td = document.createElement('td');
-     title_td.textContent = element.Product;
-     tr.appendChild(title_td);
-
-      //item price
-     let price_td = document.createElement("td");
-     price_td.textContent = element.Price+" L.E";
-     tr.appendChild(price_td);
-
-     //items quantity
-     let qty_td = document.createElement('td');
-     qty_td.innerHTML =`<i data-id="${ID}" class="decrease fas fa-minus-circle" aria-hidden="true" ></i>
-                        <span >${element.Quantity}</span>
-                        <i data-id="${ID}" class="increase fas fa-plus-circle" aria-hidden="true" ></i>`;
-     tr.appendChild(qty_td);
-
-
-       //total per item  
-     let total_td = document.createElement("td");
-     total_td.textContent = element.Price * element.Quantity +" L.E";
-     tr.appendChild(total_td);
-     totalPriceOfAllObjectsInCart+= parseInt(element.Price * element.Quantity);
-     tbody.appendChild(tr);
-     console.log(totalPriceOfAllObjectsInCart);
-    
-     
-});
-console.log(totalPriceOfAllObjectsInCart);
-
-
-
- cartPrice=localStorage.getItem('totalPriceofAllObjectsInCart');
-console.log(cartPrice);
-
-let b_title = document.createElement('div');
-if(parseInt(cartPrice)>0){
-    b_title.innerHTML = `<div class="total col-lg-6 col-md-6 col-12">
-    <h5>Total</h5>
-    <div class="title d-flex justify-content-between">
-        <h6>Subtotal</h6>
-        <p>${parseInt(cartPrice)} L.E</p>
-    </div>
-   
-    <div class="title d-flex justify-content-between">
-        <h6>Shipping</h6>
-        <p>50 L.E</p>
-    </div>
-    <hr class="second-hr">
-    <div class="title d-flex justify-content-between">
-        <h6>Total</h6>
-        <p>${parseInt(cartPrice)+50} L.E</p>
-    </div> 
-    <a href="index.html"><button  class="home_btn btn btn-primary mt-1 ml-auto" > <i class="fa fa-arrow-left" aria-hidden="true"></i> Back To Home</button></a> 
-    <a href="checkout.html"><button class="proceed_btn btn btn-primary mt-1 ">Proceed To checkout</button></a> 
-   </div>`;
-   bsection.appendChild(b_title);
-}
-if(parseInt(cartPrice)==0){
-    b_title.innerHTML = `<div class="total col-lg-6 col-md-6 col-12">
-    <h5>Total</h5>
-    <div class="title d-flex justify-content-between">
-        <h6>Subtotal</h6>
-        <p>0 L.E</p>
-    </div>
-   
-    <div class="title d-flex justify-content-between">
-        <h6>Shipping</h6>
-        <p>50 L.E</p>
-    </div>
-    <hr class="second-hr">
-    <div class="title d-flex justify-content-between">
-        <h6>Total</h6>
-        <p>${parseInt(cartPrice)+50} L.E</p>
-    </div> 
-    <a href="index.html"><button  class="home_btn btn btn-primary mt-1 ml-auto" > <i class="fa fa-arrow-left" aria-hidden="true"></i> Back To Home</button></a> 
-    <a href="checkout.html"><button class="proceed_btn btn btn-primary mt-1 ">Proceed To checkout</button></a> 
-   </div>`;
-   bsection.appendChild(b_title);
+function inc(index) {
+    tempArr[index].quantity += 1;
+    localStorage.setItem("cart", JSON.stringify(tempArr));
+    setPageContent();
 }
 
-  console.log(cartArr);
- 
-  
-  
+function cartRedirect() {
+    localStorage.setItem("totalPrice", totalPriceOfAllObjectsInCart);
+    window.location = './checkout.html';
 }
+
+setPageContent();
